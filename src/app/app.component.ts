c25b118147ec0a18abbf0c4dea312541eb338788
arthur888 <jjj8888111@gmail.com>
2017-04-23 02:48:48 +0800

    initial_project

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <div class="container">
+      <h1>Reactive Forms</h1>
+      <hero-list></hero-list>
+    </div>`
+})
+export class AppComponent {
+
+}
